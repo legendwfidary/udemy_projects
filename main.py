bce57b147e0bd0395d9f3f87a72f4510d049b9bd
Karthik Sriram <83937519+legendwfidary@users.noreply.github.com>
2022-01-01 09:02:38 +0000

    basic dictionary learning

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-weight = float(input('please enter your height in kg: '))
-height = float(input('please enter your height in m: '))
+# learning about dictionaries.
 
-bmi = weight / (height ** 2)
-bmi = round(bmi)
+programming_dictionary = {
+  'bug': 'an error in the program that prevents the program from running.',
+  'function': 'a piece of code that you can easily call over and over again.',
+  'loop': 'the action of doing something over and over again.'
+}
 
+# a key is the variable name that holds the info and the item is the info, if u tap into dictionary and specify a key name, you can get that key item{info}
+# how to retrieve specific info abut differentkeys in dictionary.
+# print(programming_dictionary['bug'])
 
-if bmi  < 18.5:
-  print(f'your bmi is {bmi}, you are underweight.')
-elif bmi < 25:
-  print(f'your bmi is {bmi}, you are normal weight.')
-elif bmi < 30:
-  print(f'your bmi is {bmi}, you are overweight.')
-elif bmi < 35:
-  print(f'your bmi is {bmi}, you are obese.')
+#an empty dictionary
+empty_dictionary = {}
 
-else:
-  print(f'your bmi is {bmi}, you are clinically obese.')
+#erasing everything from a dictionary.
+#programming_dictionary = {}
+#print(programming_dictionary)\
+
+#editing item in dictionary
+programming_dictionary['bug'] =  'a moth in your computer.'
+#print(programming_dictionary)
+
+#looping through a dictionary
+for key in programming_dictionary:
+  print(key) # will obtain all the keys
+  print(programming_dictionary[key]) # will obtain keys and items
+  #btw, when u do thisk it just prints out the keys.
